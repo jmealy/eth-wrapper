53dc5c0e0fc7fe0f1945546d391e767a600e4d66
James Mealy <james@safe.global>
2024-04-19 16:52:04 +0200

    fix: enable CORS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
-
 import css from "./page.module.css";
 import { InputAdornment, Paper, TextField, Typography } from "@mui/material";
 import { useState, ChangeEvent, useCallback } from "react";
 import InputForm from "@/components/Input";
+import { useIsSafeApp } from "@/hooks/useIsSafeApp";
 
 export default function Home() {
+  const isSafeApp = useIsSafeApp();
+
+  if (!isSafeApp) {
+    return <Typography>This app can only be run as a Safe App</Typography>;
+  }
+
   return (
     <>
       <InputForm isWrap={true} />
